7a7c6571d9905ac7e959220b0accaa88f70a6969
Hēsperus <hesprs@outlook.com>
2026-03-22 17:41:46 +0800

    fix(record): fix duplicated pull/push caused by false record update

--- src/utils/request-url.ts
+++ src/utils/request-url.ts
@@ -1,23 +1,6 @@
 import { requestUrl as req, type RequestUrlParam, type RequestUrlResponse } from 'obsidian';
 import logger from './logger';
 
-// TODO: delete
-function toDebugHeaders(headers?: Record<string, string>) {
-	if (!headers) return undefined;
-
-	return Object.fromEntries(
-		Object.entries(headers).map(([key, value]) => {
-			if (key.toLowerCase() === 'authorization') return [key, '<retracted>'];
-			return [key, value];
-		}),
-	);
-}
-
-// TODO: delete
-function getResponsePreview(res: RequestUrlResponse) {
-	return res.text.slice(0, 300);
-}
-
 class RequestUrlError extends Error {
 	constructor(public res: RequestUrlResponse) {
 		super(`${res.status}: ${res.text}`);
@@ -46,63 +29,7 @@ export default async function requestUrl(p: RequestUrlParam | string) {
 					},
 				};
 
-	// const res = await req(params);
-
-	// TODO: delete
-	logger.debug(
-		'requestUrl request started',
-		{
-			url: params.url,
-			method: params.method ?? 'GET',
-			contentType: params.contentType,
-			headers: toDebugHeaders(params.headers),
-			bodyType:
-				params.body instanceof ArrayBuffer
-					? 'arrayBuffer'
-					: typeof params.body === 'string'
-						? 'string'
-						: params.body === undefined
-							? 'undefined'
-							: typeof params.body,
-			bodyLength:
-				typeof params.body === 'string'
-					? params.body.length
-					: params.body instanceof ArrayBuffer
-						? params.body.byteLength
-						: undefined,
-		},
-		{ category: 'network.request' },
-	);
-	// TODO: delete
-	let res: RequestUrlResponse;
-	try {
-		res = await req(params);
-	} catch (error) {
-		// TODO: delete
-		logger.debug(
-			'requestUrl request threw before response',
-			{
-				url: params.url,
-				method: params.method ?? 'GET',
-				error,
-			},
-			{ category: 'network.request' },
-		);
-		throw error;
-	}
-	// TODO: delete
-	logger.debug(
-		'requestUrl response received',
-		{
-			url: params.url,
-			method: params.method ?? 'GET',
-			status: res.status,
-			headers: toDebugHeaders(res.headers),
-			textLength: res.text.length,
-			textPreview: getResponsePreview(res),
-		},
-		{ category: 'network.request' },
-	);
+	const res = await req(params);
 
 	if (res.status >= 400) {
 		if (!isExpectedNotFoundResponse(p, res))
