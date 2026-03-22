f500554b0d295d7bdcc57a66fb03c90be5138834
Hēsperus <hesprs@outlook.com>
2026-03-22 18:55:40 +0800

    fix(request): normalize remote dir to address remote dir deletion error (#24)

--- src/sync/tasks/remove-remote-recursively.task.ts
+++ src/sync/tasks/remove-remote-recursively.task.ts
@@ -1,10 +1,11 @@
+import { normalizeRemoteDir } from '~/platform/path/remote-path';
 import logger from '~/utils/logger';
 import { BaseTask, toTaskError } from './task.interface';
 
 export default class RemoveRemoteRecursivelyTask extends BaseTask {
 	async exec() {
 		try {
-			await this.webdav.deleteFile(this.remotePath);
+			await this.webdav.deleteFile(normalizeRemoteDir(this.remotePath));
 			await this.syncRecord.removeRemoteRecordSubtree(this.remotePath);
 			return { success: true, skipRecord: true } as const;
 		} catch (e) {
