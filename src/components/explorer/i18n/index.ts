56cc8965da215919db7cb80a0237577db9a02f0c
Hēsperus <hesprs@outlook.com>
2026-04-09 23:26:26 +0800

    fix(mkdir): remove unnecessary recursive dir creation (#66)
    - Removed `recursive: true` in mkdir remote task
    - Add Russian translations in webdav explorer i18n

--- src/components/explorer/i18n/index.ts
+++ src/components/explorer/i18n/index.ts
@@ -1,14 +1,17 @@
 import * as i18n from '@solid-primitives/i18n';
 import { createResource, createSignal } from 'solid-js';
 import en from './en';
+import ru from './ru';
 import zh from './zh';
 
-export type Locale = 'zh' | 'en';
+type Locale = 'zh' | 'en' | 'ru';
 
 export function toLocale(language: string) {
 	switch (language.split('-')[0].toLowerCase()) {
 		case 'zh':
 			return 'zh';
+		case 'ru':
+			return 'ru';
 		default:
 			return 'en';
 	}
@@ -20,6 +23,8 @@ const [dict] = createResource(locale, (locale) => {
 	switch (locale) {
 		case 'zh':
 			return i18n.flatten(zh);
+		case 'ru':
+			return i18n.flatten(ru);
 		default:
 			return i18n.flatten(en);
 	}
